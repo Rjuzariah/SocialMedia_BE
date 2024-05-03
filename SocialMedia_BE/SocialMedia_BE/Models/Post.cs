8ea3c05708086847ed5ddd8df7136904395d40bc
Juzariah <riajuzariah@gmail.com>
2024-05-03 13:30:15 +0700

    init commit 2

---
+++ SocialMedia_BE/SocialMedia_BE/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialMedia_BE.Models
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string? Description { get; set; }
+		public int Owner { get; set; }
+		public DateTime CreatedDateTime { get; set; }
+		public DateTime UpdatedDateTime { get; set; }
+	}
+}
---
+++ SocialMedia_BE/SocialMedia_BE/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SocialMedia_BE.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SocialMedia_BEContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SocialMedia_BEContext") ?? throw new InvalidOperationException("Connection string 'SocialMedia_BEContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
